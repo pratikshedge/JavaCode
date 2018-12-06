15b6475f23bdbd04b7c46fa3d10291d4969da971
Pratik Prakash Shedge <44803000+pratikshedge@users.noreply.github.com>
2018-12-06 20:30:29 +0530

    The Ultimate Number Pattern Coding

---
+++ NumberPatterns.java
@@ -0,0 +1,484 @@
+package Patterns;
+
+public class NumberPatterns {
+	public static void main(String[] args)
+	{ System.out.println("-----Pattern1-----");
+		for(int i=1;i<=1;i++)
+		{
+			for(int j=5;j>=i;j--)
+			{
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+		System.out.println();
+		{ System.out.println("-----Pattern2-----");
+			for(int i=1;i<=5;i++)
+			{
+				for(int j=1;j<=i;j++)
+				{
+					System.out.print(j);
+				}
+				System.out.println();
+			}
+		}
+		System.out.println();
+		{ System.out.println("-----Pattern3-----");
+			for(int i=1;i<=5;i++)
+			{
+				for(int j=1;j<=i;j++)
+				{
+					System.out.print(i);
+				}
+				System.out.println();
+			}
+		}
+		System.out.println();
+		{ System.out.println("-----Patter4-----");
+			for(int i=1;i<=5;i++)
+			{
+				for(int j=5;j>=i;j--)
+				{
+					System.out.print(" ");
+				}
+				for(int k=1;k<=i;k++)
+				{
+					System.out.print(k);
+				}
+				System.out.println();
+			}
+		}
+		System.out.println();
+		{ System.out.println("-----Pattern5-----");
+			for(int i=1;i<=5;i++)
+			{
+				for(int j=5;j>=i;j--)
+				{
+					System.out.print(" ");
+				}
+				for(int k=1;k<=i;k++)
+				{
+					System.out.print(i);
+				}
+				System.out.println();
+			}
+		}
+		System.out.println();
+		{ System.out.println("-----Pattern6-----");
+		int num=1;	
+		for(int i=1;i<=5;i++)
+			{
+				for(int j=1;j<=i;j++)
+				{
+					System.out.print(num+" ");
+					num=num+1;
+				}
+				System.out.println();
+			}
+		}
+		System.out.println();
+		{ System.out.println("-----Pattern7-----");
+			for(int i=1;i<=6;i++)
+			{
+				for(int j=1;j<=i;j++)
+				{
+					System.out.print(j);
+				}
+				System.out.println();
+			}
+			int num=1;
+			for(int i=1;i<=6;i++)
+			{
+				for(int j=6;j>=i;j--)
+				{
+					System.out.print(num+"");
+					num=num+1;
+				}
+				num=1;
+				System.out.println();
+			}
+		}
+		System.out.println();
+		{ System.out.println("-----Pattern8-----");
+		int num=7;	
+		for(int i=1;i<=7;i++)
+			{
+				for(int j=7;j>=i;j--)
+				{
+					System.out.print(num+"");
+					num=num-1;
+				}
+				num=7;
+				System.out.println();
+			}
+		}
+		System.out.println();
+		{ System.out.println("-----Pattern9-----");
+		int num=1;	
+		for(int i=1;i<=7;i++)
+			{
+				for(int j=7;j>=i;j--)
+				{
+					System.out.print(num+"");
+					num=num+1;
+				}
+				num=1;
+				System.out.println();
+			}
+		}
+		System.out.println();
+		{ System.out.println("-----Pattern10-----");
+		int num=7;	
+		for(int i=1;i<=7;i++)
+			{
+				for(int j=1;j<=i;j++)
+				{
+					System.out.print(num+"");
+					num=num-1;
+				}
+				num=7;
+				System.out.println();
+			}
+		}
+		System.out.println();
+		{ System.out.println("-----Pattern11-----");
+		int num=1;	
+		for(int i=1;i<=7;i++)
+			{
+				for(int j=7;j>=i;j--)
+				{
+					System.out.print(num+"");
+					num=num+1;
+				}
+				num=1;
+				System.out.println();
+			}
+		}
+		{ 
+			
+		for(int i=1;i<=7;i++)
+			{
+				for(int j=1;j<=i;j++)
+				{
+					System.out.print(j);
+					
+				}
+				
+				System.out.println();
+			}
+		}
+		System.out.println();
+		{ System.out.println("-----Pattern12-----");
+		int num=1;	
+		for(int i=1;i<=7;i++)
+			{
+				for(int j=1;j<=i;j++)
+				{
+					System.out.print(" ");
+				}
+				for(int k=7;k>=i;k--)
+				{
+					System.out.print(num+"");
+					num=num+1;
+				}
+				num=1;
+				System.out.println();
+			}
+		}
+		{
+		int num=1;	
+		for(int i=1;i<=7;i++)
+			{
+				for(int j=7;j>=i;j--)
+				{
+					System.out.print(" ");
+				}
+				for(int k=1;k<=i;k++)
+				{
+					System.out.print(num+"");
+					num=num+1;
+				}
+				num=1;
+				System.out.println();
+			}
+		}
+		System.out.println();
+		{ System.out.println("-----Pattern13-----");
+		int num=1;	
+		for(int i=1;i<=7;i++)
+			{
+				for(int j=7;j>=i;j--)
+				{
+					System.out.print(num+"");
+					num=num+1;
+				}
+				num=1;
+				System.out.println();
+			}
+		}
+		{ 
+			
+		for(int i=1;i<=7;i++)
+			{
+				for(int j=1;j<=i;j++)
+				{
+					System.out.print(j);
+					
+				}
+				
+				System.out.println();
+			}
+		}
+		{ 
+		int num=1;	
+		for(int i=1;i<=7;i++)
+			{
+				for(int j=1;j<=i;j++)
+				{
+					System.out.print(" ");
+				}
+				for(int k=7;k>=i;k--)
+				{
+					System.out.print(num+"");
+					num=num+1;
+				}
+				num=1;
+				System.out.println();
+			}
+		}
+		{
+		int num=1;	
+		for(int i=1;i<=7;i++)
+			{
+				for(int j=7;j>=i;j--)
+				{
+					System.out.print(" ");
+				}
+				for(int k=1;k<=i;k++)
+				{
+					System.out.print(num+"");
+					num=num+1;
+				}
+				num=1;
+				System.out.println();
+			}
+		}
+		System.out.println();
+		{ System.out.println("-----Pattern14-----");
+		int num=1;
+			for(int i=1;i<=6;i++)
+			{
+				for(int j=1;j<=i;j++)
+				{
+					System.out.print(j);
+				}
+				for(int k=11;k>=i*2;k--)
+				{
+					System.out.print(" ");
+				}
+				for(int l=1;l<=i;l++)
+				{
+					System.out.print(num+"");
+					num=num+1;
+				}
+				num=1;
+				System.out.println();
+			}
+		}
+		System.out.println();
+		{ System.out.println("-----Pattern15-----");
+		int num=1;
+			for(int i=1;i<=6;i++)
+			{
+				for(int j=6;j>=i;j--)
+				{
+					System.out.print(num+"");
+					num=num+1;
+				}
+				num=1;
+				for(int k=2;k<=(i*2-1);k++)
+				{
+					System.out.print(" ");
+				}
+				for(int l=6;l>=i;l--)
+				{
+					System.out.print(num+"");
+					num=num+1;
+				}
+				num=1;
+				System.out.println();
+			}
+		}
+		System.out.println();
+		{ System.out.println("-----Pattern16-----");
+		int num=1;
+			for(int i=1;i<=6;i++)
+			{
+				for(int j=1;j<=i;j++)
+				{
+					System.out.print(j);
+				}
+				for(int k=11;k>=i*2;k--)
+				{
+					System.out.print(" ");
+				}
+				for(int l=1;l<=i;l++)
+				{
+					System.out.print(num+"");
+					num=num+1;
+				}
+				num=1;
+				System.out.println();
+			}
+		}
+		{ 
+		int num=1;
+			for(int i=1;i<=6;i++)
+			{
+				for(int j=6;j>=i;j--)
+				{
+					System.out.print(num+"");
+					num=num+1;
+				}
+				num=1;
+				for(int k=2;k<=(i*2-1);k++)
+				{
+					System.out.print(" ");
+				}
+				for(int l=6;l>=i;l--)
+				{
+					System.out.print(num+"");
+					num=num+1;
+				}
+				num=1;
+				System.out.println();
+			}
+		}
+		System.out.println();
+		{ System.out.println("-----Pattern17-----");
+		
+			for(int i=1;i<=6;i++)
+			{
+				for(int j=i;j>=1;j--)
+				{
+					System.out.print(j+"");
+				}
+			
+				System.out.println();
+			}
+		}
+		System.out.println();
+		{ System.out.println("-----Pattern18-----");
+		    int num=1;
+			for(int i=1;i<=4;i++)
+			{
+				for(int j=3;j>=i;j--)
+				{
+					System.out.print(" ");
+				}
+				for(int k=1;k<=(2*i-1);k++)
+				{
+					System.out.print(num+"");
+					num=num+1;
+				}
+				num=1;
+				System.out.println();
+			}
+		}
+		System.out.println();
+		{ System.out.println("-----Pattern19-----");
+		    int num=1;
+			for(int i=1;i<=4;i++)
+			{
+				for(int j=1;j<=i;j++)
+				{
+					System.out.print(" ");
+				}
+				for(int k=8;k>=2*i;k--)
+				{
+					System.out.print(num+"");
+					num=num+1;
+				}
+				num=1;
+				System.out.println();
+			}
+		}
+		System.out.println();
+		{ System.out.println("-----Pattern20-----");
+		    int num=1;
+			for(int i=1;i<=4;i++)
+			{
+				for(int j=4;j>=i;j--)
+				{
+					System.out.print(" ");
+				}
+				for(int k=1;k<=(2*i-1);k++)
+				{
+					System.out.print(num+"");
+					num=num+1;
+				}
+				num=1;
+				System.out.println();
+			}
+		}
+		{ 
+	    int num=1;
+		for(int i=1;i<=4;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(" ");
+			}
+			for(int k=8;k>=2*i;k--)
+			{
+				System.out.print(num+"");
+				num=num+1;
+			}
+			num=1;
+			System.out.println();
+		}
+	}
+		System.out.println();
+		{ System.out.println("-----Pattern21-----");
+        int num=1;
+		for(int i=1;i<=7;i++)
+			{
+				for(int j=6;j>=i;j--)
+				{
+					System.out.print(num+"");
+				}
+				for(int k=1;k<=i;k++)
+				{
+					System.out.print(i);
+				}
+			
+				System.out.println();
+			}
+		}
+		System.out.println();
+		{ System.out.println("-----Pattern22-----");
+        int j;
+		for(int i=1;i<=7;i++)
+			{   
+				for(j=1;j<=i;j++)
+				{
+			
+			
+				if(j%2==0)
+				{
+					System.out.print(0);
+				}
+				else
+				{
+					System.out.print(1);
+				}	
+				}
+				System.out.println();
+			}
+		}
+	}
+}
+	
+
+	
+
